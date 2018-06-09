143626ad11b87f86057a2bb3522e90c5a2253647
allseen <allseen@gmail.com>
2018-06-09 22:21:21 +0300

    for

---
+++ 1/1.3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower=0;
+    upper=300;
+    step=20;
+
+    celsius=lower;
+    printf("Celsius to Fahrenheit\n\n");
+    printf("%3s %7s\n", "cel", "fahr");
+    while(fahr<=upper)
+    {
+        //celsius=(5.0/9.0)*(fahr-32.0);
+        fahr=(celsius/5*9)+32;
+        printf("%3.0f %6.0f\n", celsius, fahr);
+        celsius=celsius+step;
+    }
+}
