20568903ff02104f6580fb0b2d8edbecd5a4b310
allseenn <allseen@gmail.com>
2023-10-31 13:52:59 +0300

    01.Tasks

---
+++ 01.Tasks/task02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Let’s\n");
+    printf("    go\n");
+    printf("       to walk\n");
+    return 0;
+}
---
+++ 01.Tasks/task03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf(" HH HH\n");
+    printf(" ZZZZZ\n");
+    return 0;
+}
+    
+   
+  
+ 
+  
+  
